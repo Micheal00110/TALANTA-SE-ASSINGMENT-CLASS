ea55333214f6f9696791097acc4bca9811b3b2b7
Your GitHub Username <your.email@example.com>
2025-08-01 10:15:10 +0300

    added index.js file

---
+++ week6/index.js
@@ -0,0 +1,8 @@
+let day = "Monday";
+if (day === "Saturday" || day === "Sunday") {
+    console.log("It's the weekend!");
+} else if (day === "Monday" || day === "Friday") {
+    console.log("It's a work day!");
+} else {
+    console.log("It's a work day!");
+}
